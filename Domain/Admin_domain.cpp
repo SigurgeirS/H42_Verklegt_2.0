90664a9162e15915bed27882e7c4f263981d86c4
Sigurgeir <sigurgeir16@ru.is>
2017-12-15 01:56:03 +0000

    cpypasta

--- Domain/Admin_domain.cpp
+++ Domain/Admin_domain.cpp
@@ -8,6 +8,7 @@ Admin_domain::Admin_domain()
 ///Gets a cilumn from a line
 string Admin_domain::get_column(string line, int column)
 {
+    
     stringstream stream;
     string found = "";
     stream << line;
@@ -20,6 +21,7 @@ string Admin_domain::get_column(string line, int column)
 
 ///Topping check
 void Admin_domain::check_topping_count(int counter) throw (InvalidToppingCount){
+    //checking how many toppings
      if(cin.fail()){
         clear_input_stream();
         throw InvalidToppingCount();
@@ -37,104 +39,115 @@ void Admin_domain::check_topping_count(int counter) throw (InvalidToppingCount){
 }*/
 
 void Admin_domain::check_topping_Name(Topping& topping){
+    //checking topping name
      string ToppingName = topping.get_name();
      for(unsigned int i = 0; i < ToppingName.length(); i++){
           if(isdigit(ToppingName[i])){
              clear_input_stream();
-          throw InvalidToppingName();
+          throw InvalidToppingName(); //Throwing exception
      }
      }
 }
 
 void Admin_domain::check_topping_Price(Topping& topping){
+    //checking price for topping
      if(cin.fail()){
         clear_input_stream();
-        throw InvalidToppingPrice();
+        throw InvalidToppingPrice();//Throwing exception
      }
 }
 
 ///Base check
 void Admin_domain::check_base_count(int counter){
+    //checking how many bases
      if(cin.fail()){
         clear_input_stream();
-        throw InvalidBaseCount();
+        throw InvalidBaseCount();//throwing exception
      }
 }
 
 void Admin_domain::check_base_name(Base& base){
+    //checking the name for the base
     for(unsigned int i = 0; i < base.get_name().length(); i++){
      if(isdigit(base.get_name()[i])){
-        throw InvalidBaseName();
+        throw InvalidBaseName();// Throwing exception if base name is invalid
      }
     }
 }
 
 void Admin_domain::check_base_price(Base& base){
+    //checking price for base
      if(cin.fail()){
         clear_input_stream();
-        throw InvalidBasePrice();
+        throw InvalidBasePrice();//Throwing exception if base price is invalid
      }
 }
 
-///Size check
+
 void Admin_domain::check_size_count(int counter) throw (InvalidSizeCount){
+    //size check
      if(cin.fail()){
         clear_input_stream();
-        throw InvalidSizeCount();
+        throw InvalidSizeCount();// Throwing exception for invalidSizeCount
      }
 }
 
 void Admin_domain::check_pizza_size_price(Pizza_size& pizzasize){
+    //checking price for pizza size
       if(cin.fail()){
         clear_input_stream();
-        throw Invalidsizeprice();
+        throw Invalidsizeprice();//Throwing exception
      }
 }
 
 void Admin_domain::check_pizza_size(Pizza_size& pizzasize){
 
-     if(!isdigit(pizzasize.get_size())){
+/*     if(!isdigit(pizzasize.get_size())){
         clear_input_stream();
         throw InvalidSize();
-     }
+     }*/
 }
 
 ///Location check
 void Admin_domain::check_location_count(int counter){
+    //checking location
      if(cin.fail()){
         clear_input_stream();
-        throw InvalidLocationCount();
+        throw InvalidLocationCount();//throwing exception for location
      }
 }
 void Admin_domain::check_location_name(Location& location){
-
+    //checking if the name is right
     for(unsigned int i = 0; i < location.get_name().length(); i++){
           if(isdigit(location.get_name()[i])){
              clear_input_stream();
-          throw InvalidLocationName();
+          throw InvalidLocationName(); // throwing exception if location name is invalid
      }
 }
 }
-///MenuCheck
+
 void Admin_domain::check_menu_count(int counter){
+    //menu check
      if(cin.fail()){
         clear_input_stream();
-        throw InvalidMenuCount();
+        throw InvalidMenuCount();//throwing exception
      }
 }
 
 void Admin_domain::check_menu_name(Pizza_menu& pizzamenu){
+    //checking if the name is right
          for(unsigned int i = 0; i < pizzamenu.get_name().length(); i++){
           if(isdigit(pizzamenu.get_name()[i])){
              clear_input_stream();
-          throw InvalidPizzaMenuName();
+          throw InvalidPizzaMenuName(); //throwing exception if it's invalid
      }
 }
 }
 
-///Takes a line and finds the id
+
 string Admin_domain::find_ID(string line)
 {
+    ///Takes a line and finds the id
     char splitchar = ',';
     stringstream stream(line);
     string ID = "";
@@ -142,53 +155,15 @@ string Admin_domain::find_ID(string line)
     return ID;
 }
 
-/*Record toppings to a file*/
-void Admin_domain::record_toppings(Topping& topping)
-{
-    PizzaRepository pizzarepo;
-    this->check_topping_Price(topping);
-    this->check_topping_Name(topping);
-    pizzarepo.storeToppings(topping);
-}
-
-void Admin_domain::record_base(Base& base)
-{
-    PizzaRepository pizzarepo;
-    this->check_base_name(base);
-    this->check_base_price(base);
-    pizzarepo.storeBase(base);
-}
-
-void Admin_domain::record_menu(Pizza_menu& pizzamenu,string line)
-{
-    Pizza_menuRepository menu_repo;
-    string ID = this->find_ID(line);
-    this->check_menu_name(pizzamenu);
-    menu_repo.storemenu(ID,pizzamenu);
-}
-
-void Admin_domain::record_size(Pizza_size& pizza_size)
-{
-    PizzaRepository pizzarepo;
-    this->check_pizza_size_price(pizza_size);
-    this->check_pizza_size(pizza_size);
-    pizzarepo.storeSize(pizza_size);
-}
-
 vector<string> Admin_domain::read_all_toppings()
+// reads all toppings
 {
     Find_line findline;
     return findline.retrive_all_items("pizza_topping.txt");
 }
 
-void Admin_domain::record_location(Location& location)
-{
-    LocationRepository locationrepo;
-    this->check_location_name(location);
-    locationrepo.storelocation(location);
-}
-
 void Admin_domain::clear_input_stream(){
+    //clears the input stream
         cin.clear();
         cin.ignore();
 }
